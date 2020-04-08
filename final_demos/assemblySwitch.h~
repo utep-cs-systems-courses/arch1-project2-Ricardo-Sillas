e00b6c7e7f5bd5e75141ec6e324e26fb98efee72
Ricardo-Sillas <rrsillas@miners.utep.edu>
2020-04-07 22:13:28 -0600

    switch in assembly

---
+++ final_demos/assemblySwitch.h~
@@ -0,0 +1,6 @@
+#ifndef assemblySwitch_included
+#define assemblySwitch_included
+
+void the_switch(void);
+
+#endif
---
+++ final_demos/assemblySwitch.s
@@ -0,0 +1,30 @@
+	.arch msp430g2553
+
+	.data
+color:
+	.word 0
+
+	.text
+
+jt:
+	.word option1
+	.word option2
+	.global the_switch
+
+the_switch:
+	mov &color, r12		;Move options into r12
+	add r12, r12		;r12 * 2
+	mov jt(r12), r0
+
+option1:
+	call #toggle_red	;toggle red
+	mov #1, &color		;Setting to option2
+	jmp out			;go to out
+
+option2:
+	call #toggle_green	;toggle green
+	mov #0, &color		;color option1
+	jmp out			;go to out
+
+out:
+	pop r0			;returning
